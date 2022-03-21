3b23299b32f1ee48012a52256f90dd20a7dcd06f
Richard-DEPIERRE <richard.habimana@epitech.eu>
2022-03-21 09:32:51 +0100

    update concerts

--- assets/javascripts/concert.js
+++ assets/javascripts/concert.js
@@ -1,7 +1,6 @@
 var events = [
-  ['The SonarMen @Gazette Café', 'Montpellier - 34', 'SAM., 12 FÉVR., 2022 À 20:00', 'https://www.facebook.com/events/409456157252583/'],
-  ['The SonarMen @Théâtre de Nathalie', 'Abais - 30', 'VEN., 4 MARS, 2022 À 20:30', 'https://www.facebook.com/events/1070714730447928/'],
-  ['The SonarMen St Patrick@Arène des bières', 'Marguerittes - 30', 'EU., 17 MARS, 2022 À 20:00', 'https://www.facebook.com/events/353015296643933/'],
+  [`The SonarMen (en duo) @O'Varieties`, 'St Rémy de Provence - 13', 'SAM., 2 AVR., 2022 À 20:45', 'https://www.facebook.com/events/318366940313116/'],
+  [`The O'liver Pub Lattes`, 'Lattes - 34', 'SAM., 9 AVR., 2022 À 21:30', 'https://www.facebook.com/events/991133435109085/'],
   [`The SonarMen @Paddy Mullin's`, 'Arles - 13', 'SAM., 16 AVR., 2022 À 22:00', 'https://www.facebook.com/events/484441465909338/'],
   ['The SonarMen @Song à Sète', 'Sète - 34', 'SAM., 28 MAI, 2022 À 21:00', 'https://www.facebook.com/events/470152418169181/'],
   ['The SonarMen @Le Vigan (évènement privé motoclub)', 'Le Vigan - 30', 'SAM., 11 JUIN, 2022 À 20:30', 'https://www.facebook.com/events/616446932914463/'],
@@ -32,6 +31,10 @@ console.log(str);
 document.getElementById("listEvents").innerHTML = str;
 
 var passedEvents = [
+  ['The SonarMen @Le Piège', 'Uzès - 30', 'VEN., 18 MARS 2022 À 20:30', 'https://www.facebook.com/events/672000337252322/'],
+  ['The SonarMen St Patrick@Arène des bières', 'Marguerittes - 30', 'DIM., 17 MARS, 2022 À 20:00', 'https://www.facebook.com/events/353015296643933/'],
+  ['The SonarMen @Théâtre de Nathalie', 'Abais - 30', 'VEN., 4 MARS, 2022 À 20:30', 'https://www.facebook.com/events/1070714730447928/'],
+  ['The SonarMen @Gazette Café', 'Montpellier - 34', 'SAM., 12 FÉVR., 2022 À 20:00', 'https://www.facebook.com/events/409456157252583/'],
   ['The SonarMen @La Voûte', 'Avèze - 30', 'SAM., 11 DÉC. 2021', 'https://www.facebook.com/events/409456157252583/'],
   ['The SonarMen @ Seven Hills', 'Nîmes -30', 'VEN., 10 DÉC. 2021', 'https://www.facebook.com/events/1070714730447928/'],
   [`The SonarMen @O'Varieties`, 'St Rémy de Provence -13', 'SAM., 4 DÉC. 2021', 'https://www.facebook.com/events/353015296643933/'],
